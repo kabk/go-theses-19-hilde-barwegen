2500aedd797034d5a195a64f50a0faba9ba13991
HildeBarwegen <onions@Daniels-MacBook-Pro-3.local>
2019-01-08 20:19:53 -0600

    beta website

---
+++ build/js/main.js
@@ -0,0 +1,246 @@
+"use strict";
+
+var filterTable = function filterTable() {
+	$(".filter").click(function () {
+		var filterCat = $(this).attr("filter");
+		console.log(filterCat);
+
+		$(".entry").each(function () {
+			var $this = $(this);
+			$this.hide();
+			var entryCat = $this.attr("chap");
+			entryCat = entryCat.split(" ");
+
+			for (var i = 0; i < entryCat.length; i++) {
+				if (entryCat[i] == filterCat) {
+					$this.show();
+				}
+			}
+		});
+	});
+
+	$(".clearAll").click(function () {
+		$(".entry").show();
+	});
+};
+
+var rowCount = function rowCount() {
+	var len = $(".entry").length;
+	$(".entryCount").text(len);
+};
+
+var entryPage = function entryPage() {
+	var headerH = $("header").outerHeight();
+
+	$(".pageLink").click(function () {
+		console.log();
+		var table = "<table id=\"singleTable\">\n\t\t\t\t<tr>\n\t\t\t\t\t<th>Index</th>\n\t\t\t\t\t<th>Name</th>\n\t\t\t\t\t<th>Date</th>\n\t\t\t\t\t<th>Source</th>\n\t\t\t\t\t<th>Author</th>\n\t\t\t\t\t<th>Image</th>\n\t\t\t\t\t<th>Upvotes / Likes</th>\n\t\t\t\t</tr>\n\t\t\t</table>";
+		var row = $(this).parent().parent().clone();
+		var content = row.find(".content").clone();
+		var entryPageBtns = "<div class=\"entryPageBtns\"><span class=\"button back\">Back</span> <span class=\"right\"><span class=\"button prev\">< Previous</span>&nbsp;&nbsp;&nbsp;<span class=\"button next\">Next ></span></span></div>";
+		$("#mainPage").hide();
+		$("#entryPage").show().append(entryPageBtns).append(table);
+		$("#singleTable").append(row);
+		$("#entryPage").append(content);
+
+		$(".back").click(function () {
+			$("#mainPage").show();
+			$("#entryPage").empty().hide();
+		});
+	});
+};
+
+$(".entry").each(function (i) {
+	$(this).attr('ind', i);
+});
+
+var prevNext = function prevNext() {
+	var headerH = $("header").outerHeight();
+
+	$(".next").click(function () {
+		console.log('aosidj');
+		var table = "<table id=\"singleTable\">\n\t\t\t\t<tr>\n\t\t\t\t\t<th>Index</th>\n\t\t\t\t\t<th>Name</th>\n\t\t\t\t\t<th>Date</th>\n\t\t\t\t\t<th>Source</th>\n\t\t\t\t\t<th>Author</th>\n\t\t\t\t\t<th>Image</th>\n\t\t\t\t\t<th>Upvotes / Likes</th>\n\t\t\t\t</tr>\n\t\t\t</table>";
+		var row = $("entry").eq(10).parent().parent().clone();
+		var content = row.find(".content").clone();
+		var entryPageBtns = "<div class=\"entryPageBtns\"><span class=\"button back\">Back</span> <span class=\"right\"><span class=\"button prev\">< Previous</span>&nbsp;&nbsp;&nbsp;<span class=\"button next\">Next ></span></span></div>";
+		$("#mainPage").hide();
+		$("#entryPage").empty().show().append(entryPageBtns).append(table);
+		$("#singleTable").append(row);
+		$("#entryPage").append(content);
+
+		$(".back").click(function () {
+			$("#mainPage").show();
+			$("#entryPage").empty().hide();
+		});
+	});
+};
+
+var formatGraveyardEntries = function formatGraveyardEntries() {
+	$(".entry").each(function (i) {
+		if ($(this).attr("chap") == "onlineGraveyard image") {
+			$(this).find("td").eq(0).text("05." + i);
+			// numbers here wrong i think
+			var $content = $(this).find("td").eq(5);
+			var $1stPhoto = $(this).find("td").eq(7).clone();
+			var $2ndPhoto = $(this).find("td").eq(8).clone();
+			var $upvotes = $(this).find("td").eq(6);
+			$(this).find("td").eq(7).addClass('hidden');
+			$content.append('<br><br>').append($1stPhoto).append('<br>');
+			$content.append($2ndPhoto);
+			$content.addClass('content');
+			$upvotes.addClass("upvotes");
+			console.log(123123123);
+		}
+	});
+};
+
+filterTable(); // could simply output compted html and not use this function
+rowCount();
+entryPage();
+prevNext();
+formatGraveyardEntries();
+
+////// SORT ROWS //////
+var rows = [];
+
+$("#mainTable tr").each(function (i) {
+	var $html = $(this).html();
+	if (i !== 0) {
+		// let originalDate = $(this).find(".originalDate").text();
+		// originalDate = originalDate.split('.');
+		// let addedDate = $(this).find(".addedDate").text();
+		// addedDate = addedDate.split('.');
+		// let source = $(this).find(".source").text().replace(/\t|\n|\r/g, "")
+		// $(this).remove();
+		// rows.push({
+		// 	addedyear: addedDate[0],
+		// 	addedmonth: addedDate[1],
+		// 	addedday: addedDate[2],
+		// 	originalD: new Date(`${originalDate[0]}-${originalDate[1]}-${originalDate[2]}T03:00:00`),
+		// 	addedD: new Date(`${addedDate[0]}-${addedDate[1]}-${addedDate[2]}T03:00:00`),
+		// 	source: source,
+		// 	content: $html
+		// });
+		var index = $(this).find("td").eq(0).text().replace(/\s/g, "");
+		var name = $(this).find("td").eq(1).text().replace(/\s/g, "");
+		var date = $(this).find("td").eq(2).text().replace(/\s/g, "");
+		var source = $(this).find("td").eq(3).text().replace(/\s/g, "");
+		var author = $(this).find("td").eq(4).text().replace(/\s/g, "");
+		var chap = $(this).attr("chap");
+
+		rows.push({
+			index: index,
+			name: name,
+			date: date,
+			source: source,
+			author: author,
+			content: $html,
+			chap: chap
+		});
+	}
+});
+
+var sort = function sort(arr, sortByInd) {
+	$("#mainTable tr").each(function (i) {
+		if (i !== 0) {
+			$(this).remove();
+		}
+	});
+	// if (by == "originalDate") {
+	// 	arr.sort(function(a, b){
+	// 	    var dateA=new Date(a.originalD), dateB=new Date(b.originalD)
+	// 	    return dateB-dateA //sort by date ascending
+	// 	})
+	// } else if (by == "addedDate") {
+	// 	arr.sort(function(a, b){
+	// 	    var dateA=new Date(a.addedD), dateB=new Date(b.addedD)
+	// 	    return dateB-dateA //sort by date ascending
+	// 	})
+	// } else if (by == "source")  {
+	// 	arr.sort(function(a, b){
+	// 	    var nameA=a.source.toLowerCase(), nameB=b.source.toLowerCase()
+	// 	    if (nameA < nameB) //sort string ascending
+	// 	        return -1 
+	// 	    if (nameA > nameB)
+	// 	        return 1
+	// 	    return 0 //default return value (no sorting)
+	// 	})
+	// } else {
+	// 	console.log('ERROR SORTINGGGG')
+	// }
+
+	// let sortBy = keys[sortByInd]
+	if (sortByInd == 0) {
+		var compare = function compare(a, b) {
+			if (a.index < b.index) return -1;
+			if (a.index > b.index) return 1;
+			return 0;
+		};
+
+		arr.sort(compare);
+	} else if (sortByInd == 1) {
+		var _compare = function _compare(a, b) {
+			if (a.name < b.name) return -1;
+			if (a.name > b.name) return 1;
+			return 0;
+		};
+
+		arr.sort(_compare);
+	} else if (sortByInd == 2) {
+		var _compare2 = function _compare2(a, b) {
+			if (a.date < b.date) return -1;
+			if (a.date > b.date) return 1;
+			return 0;
+		};
+
+		arr.sort(_compare2);
+	} else if (sortByInd == 3) {
+		var _compare3 = function _compare3(a, b) {
+			if (a.source < b.source) return -1;
+			if (a.source > b.source) return 1;
+			return 0;
+		};
+
+		arr.sort(_compare3);
+	} else if (sortByInd == 4) {
+		var _compare4 = function _compare4(a, b) {
+			if (a.author < b.author) return -1;
+			if (a.author > b.author) return 1;
+			return 0;
+		};
+
+		arr.sort(_compare4);
+	} else {}
+
+	arr.forEach(function (el) {
+		$("#mainTable table").append("<tr class=\"entry\" chap=\"" + el.chap + "\">" + el.content + "</tr>");
+	});
+
+	filterTable(); // could simply output compted html and not use this function
+	rowCount();
+	entryPage();
+	prevNext();
+	formatGraveyardEntries();
+};
+
+$(".sortCol").each(function (i) {
+	$(this).click(function () {
+		sort(rows, i);
+	});
+});
+
+var lpad = function lpad(string, padString, length) {
+	var str = string.toString();
+	while (str.length < length) {
+		str = padString + str;
+	}return str;
+};
+
+for (var i = 0; i < 113; i++) {
+	$("#print").append("<img src=\"img/thesisScan/img00" + lpad(i, "0", 3) + ".jpg\"/>");
+}
+
+$("#printBtn").click(function () {
+
+	window.print();
+});
---
+++ src/js/main.js
@@ -0,0 +1,297 @@
+const filterTable = () => {
+	$(".filter").click(function() {
+		let filterCat = $(this).attr("filter");
+		console.log(filterCat);
+
+		$(".entry").each(function(){
+			let $this = $(this);
+			$this.hide();
+			let entryCat = $this.attr("chap");
+			entryCat = entryCat.split(" ")
+			
+			for (var i = 0; i < entryCat.length; i++) {
+				if (entryCat[i] == filterCat) {
+					$this.show();
+				}
+			}
+		})
+	});
+
+	$(".clearAll").click(function() {
+		$(".entry").show();
+	});
+};
+
+const rowCount = () => {
+	let len = $(".entry").length;
+	$(".entryCount").text(len);
+}
+
+
+const entryPage = () => {
+	let headerH = $("header").outerHeight();
+
+	$(".pageLink").click(function(){
+		console.log()
+		let table = `<table id="singleTable">
+				<tr>
+					<th>Index</th>
+					<th>Name</th>
+					<th>Date</th>
+					<th>Source</th>
+					<th>Author</th>
+					<th>Image</th>
+					<th>Upvotes / Likes</th>
+				</tr>
+			</table>`;
+		let row = $(this).parent().parent().clone();
+		let content = row.find(".content").clone();
+		let entryPageBtns = `<div class="entryPageBtns"><span class="button back">Back</span> <span class="right"><span class="button prev">< Previous</span>&nbsp;&nbsp;&nbsp;<span class="button next">Next ></span></span></div>`;
+		$("#mainPage").hide();
+		$("#entryPage").show().append(entryPageBtns).append(table);
+		$("#singleTable").append(row);
+		$("#entryPage").append(content);
+
+		$(".back").click(function(){
+			$("#mainPage").show();
+			$("#entryPage").empty().hide();
+		});
+	});
+}
+
+$(".entry").each(function(i){
+	$(this).attr('ind', i);
+});
+
+const prevNext = () => {
+	let headerH = $("header").outerHeight();
+
+	
+
+	$(".next").click(function(){
+		console.log('aosidj')
+		let table = `<table id="singleTable">
+				<tr>
+					<th>Index</th>
+					<th>Name</th>
+					<th>Date</th>
+					<th>Source</th>
+					<th>Author</th>
+					<th>Image</th>
+					<th>Upvotes / Likes</th>
+				</tr>
+			</table>`;
+		let row = $("entry").eq(10).parent().parent().clone();
+		let content = row.find(".content").clone();
+		let entryPageBtns = `<div class="entryPageBtns"><span class="button back">Back</span> <span class="right"><span class="button prev">< Previous</span>&nbsp;&nbsp;&nbsp;<span class="button next">Next ></span></span></div>`;
+		$("#mainPage").hide();
+		$("#entryPage").empty().show().append(entryPageBtns).append(table);
+		$("#singleTable").append(row);
+		$("#entryPage").append(content);
+
+		$(".back").click(function(){
+			$("#mainPage").show();
+			$("#entryPage").empty().hide();
+		});
+	});
+}
+
+const formatGraveyardEntries = () => {
+	$(".entry").each(function(i){
+		if ($(this).attr("chap") == "onlineGraveyard image") {
+			$(this).find("td").eq(0).text("05."+i)
+			// numbers here wrong i think
+			let $content = $(this).find("td").eq(5)
+			let $1stPhoto = $(this).find("td").eq(7).clone()
+			let $2ndPhoto = $(this).find("td").eq(8).clone()
+			let $upvotes = $(this).find("td").eq(6)
+			$(this).find("td").eq(7).addClass('hidden')
+			$content.append('<br><br>').append($1stPhoto).append('<br>');
+			$content.append($2ndPhoto);
+			$content.addClass('content');
+			$upvotes.addClass("upvotes");
+			console.log(123123123)
+
+		}
+	})
+}
+
+
+filterTable(); // could simply output compted html and not use this function
+rowCount();
+entryPage();
+prevNext();
+formatGraveyardEntries();
+
+
+
+
+
+
+
+
+
+////// SORT ROWS //////
+let rows = []
+
+$("#mainTable tr").each(function(i) {
+	let $html = $(this).html();
+	if (i !== 0) {
+		// let originalDate = $(this).find(".originalDate").text();
+		// originalDate = originalDate.split('.');
+		// let addedDate = $(this).find(".addedDate").text();
+		// addedDate = addedDate.split('.');
+		// let source = $(this).find(".source").text().replace(/\t|\n|\r/g, "")
+		// $(this).remove();
+		// rows.push({
+		// 	addedyear: addedDate[0],
+		// 	addedmonth: addedDate[1],
+		// 	addedday: addedDate[2],
+		// 	originalD: new Date(`${originalDate[0]}-${originalDate[1]}-${originalDate[2]}T03:00:00`),
+		// 	addedD: new Date(`${addedDate[0]}-${addedDate[1]}-${addedDate[2]}T03:00:00`),
+		// 	source: source,
+		// 	content: $html
+		// });
+		let index = $(this).find("td").eq(0).text().replace(/\s/g, "");
+		let name = $(this).find("td").eq(1).text().replace(/\s/g, "");
+		let date = $(this).find("td").eq(2).text().replace(/\s/g, "");
+		let source = $(this).find("td").eq(3).text().replace(/\s/g, "");
+		let author = $(this).find("td").eq(4).text().replace(/\s/g, "");
+		let chap = $(this).attr("chap");
+
+		rows.push({
+			index: index,
+			name: name,
+			date: date,
+			source: source,
+			author: author,
+			content: $html,
+			chap: chap
+		});
+	}
+
+})
+
+const sort = (arr, sortByInd) => {
+	$("#mainTable tr").each(function(i) {
+		if (i !== 0) {
+			$(this).remove();
+		}
+
+	})
+	// if (by == "originalDate") {
+	// 	arr.sort(function(a, b){
+	// 	    var dateA=new Date(a.originalD), dateB=new Date(b.originalD)
+	// 	    return dateB-dateA //sort by date ascending
+	// 	})
+	// } else if (by == "addedDate") {
+	// 	arr.sort(function(a, b){
+	// 	    var dateA=new Date(a.addedD), dateB=new Date(b.addedD)
+	// 	    return dateB-dateA //sort by date ascending
+	// 	})
+	// } else if (by == "source")  {
+	// 	arr.sort(function(a, b){
+	// 	    var nameA=a.source.toLowerCase(), nameB=b.source.toLowerCase()
+	// 	    if (nameA < nameB) //sort string ascending
+	// 	        return -1 
+	// 	    if (nameA > nameB)
+	// 	        return 1
+	// 	    return 0 //default return value (no sorting)
+	// 	})
+	// } else {
+	// 	console.log('ERROR SORTINGGGG')
+	// }
+
+	// let sortBy = keys[sortByInd]
+	if (sortByInd == 0) {
+		function compare(a,b) {
+		  if (a.index < b.index)
+		    return -1;
+		  if (a.index > b.index)
+		    return 1;
+		  return 0;
+		}
+		arr.sort(compare);
+	} else if (sortByInd == 1) {
+		function compare(a,b) {
+		  if (a.name < b.name)
+		    return -1;
+		  if (a.name > b.name)
+		    return 1;
+		  return 0;
+		}
+		arr.sort(compare);
+	} else if (sortByInd == 2) {
+		function compare(a,b) {
+		  if (a.date < b.date)
+		    return -1;
+		  if (a.date > b.date)
+		    return 1;
+		  return 0;
+		}
+		arr.sort(compare);
+	} else if (sortByInd == 3) {
+		function compare(a,b) {
+		  if (a.source < b.source)
+		    return -1;
+		  if (a.source > b.source)
+		    return 1;
+		  return 0;
+		}
+		arr.sort(compare);
+	} else if (sortByInd == 4) {
+		function compare(a,b) {
+		  if (a.author < b.author)
+		    return -1;
+		  if (a.author > b.author)
+		    return 1;
+		  return 0;
+		}
+		arr.sort(compare);
+	} else {}
+
+	arr.forEach(el => {
+		$("#mainTable table").append(`<tr class="entry" chap="${el.chap}">${el.content}</tr>`);
+	})
+
+	filterTable(); // could simply output compted html and not use this function
+	rowCount();
+	entryPage();
+	prevNext()
+	formatGraveyardEntries();
+}
+
+$(".sortCol").each(function(i){
+	$(this).click(function(){
+		sort(rows, i)
+	})
+})
+
+
+
+
+
+
+
+
+
+
+
+
+const lpad = (string, padString, length) => {
+    var str = string.toString();
+    while (str.length < length)
+        str = padString + str;
+    return str;
+}
+
+
+for (var i = 0; i < 113; i++) {
+	$("#print").append(`<img src="img/thesisScan/img00${lpad(i,"0",3)}.jpg"/>`);
+}
+
+$("#printBtn").click(function(){
+
+	window.print();
+
+});
